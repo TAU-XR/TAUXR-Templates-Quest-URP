0aa1510e720c8eac149bae3e307d578b4e0e9e60
talmzip <89967688+talmzip@users.noreply.github.com>
2024-01-28 19:11:39 +0200

    TXRButtonVisuals - show sequence

--- Assets/TAUXR/TXR_Interactions/TXR_Button_Touch/TXRButtonVisuals.cs
+++ Assets/TAUXR/TXR_Interactions/TXR_Button_Touch/TXRButtonVisuals.cs
@@ -1,19 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using TMPro;
 
 public class TXRButtonVisuals : MonoBehaviour
 {
     [SerializeField] private Shapes.Rectangle _backface;
     [SerializeField] private Shapes.Rectangle _stroke;
-    [SerializeField] private Transform _text;
+    [SerializeField] private TextMeshPro _text;
+
+    [Header("General")]
+    [SerializeField] private Color _backfaceColorActive;
+    [SerializeField] private Color _backfaceColorPressed;
+    [SerializeField] private Color _backfaceColorInactive;
+    [SerializeField] private Color _backfaceColorGradientHover;
+    [SerializeField] private float _strokeThickness;
+
+
+
+    [Header("Show Animation")]
+    [SerializeField] private float _showDuration;
+
+    Tween _showTween;
+
+
     void Start()
     {
-        
+        CreateShowSequence();
     }
 
     void Update()
     {
-        
+
     }
+
+    private void CreateShowSequence()
+    {
+        Color backfaceColor = _backfaceColorActive;
+        Color textColor = _text.color;
+
+        _showTween.SetAutoKill(false);
+        _showTween = DOVirtual.Float(0, 1, _showDuration, value =>
+        {
+            backfaceColor.a = value;
+            _backface.FillColorStart = backfaceColor;
+            _backface.FillColorEnd = backfaceColor;
+
+            _strokeThickness = Mathf.Lerp(0, _strokeThickness, value);
+
+            textColor.a = value;
+            _text.color = textColor;
+        });
+        _showTween.SetEase(Ease.OutCubic);
+        _showTween.Pause();
+
+
+    }
+
 }
