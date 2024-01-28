13b75d9244d3c74d63a2b10be6c537458bdd20f5
talmzip <89967688+talmzip@users.noreply.github.com>
2024-01-28 20:26:01 +0200

    Update Button Visuals script

--- Assets/TAUXR/TXR_Interactions/TXR_Button_Touch/TXRButtonVisuals.cs
+++ Assets/TAUXR/TXR_Interactions/TXR_Button_Touch/TXRButtonVisuals.cs
@@ -19,40 +19,85 @@ public class TXRButtonVisuals : MonoBehaviour
 
     [Header("Show Animation")]
     [SerializeField] private float _showDuration;
+    [SerializeField] private float _hideDuration;
 
     Tween _showTween;
+    Tween _hideTween;
 
+    private void OnValidate()
+    {
+        _showTween.Kill();
+        _hideTween.Kill();
+        InitShowHideTweens();
+    }
 
     void Start()
     {
-        CreateShowSequence();
+        InitShowHideTweens();
     }
 
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Show();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Hide();
+        }
     }
 
-    private void CreateShowSequence()
+    public void Show()
+    {
+        if (_hideTween.IsPlaying())
+        {
+            _hideTween.Pause();
+        }
+        _showTween.Restart();
+    }
+
+    public void Hide()
+    {
+        if (_showTween.IsPlaying())
+        {
+            _showTween.Pause();
+        }
+        _hideTween.Restart();
+    }
+
+    private void InitShowHideTweens()
     {
         Color backfaceColor = _backfaceColorActive;
         Color textColor = _text.color;
 
-        _showTween.SetAutoKill(false);
         _showTween = DOVirtual.Float(0, 1, _showDuration, value =>
         {
             backfaceColor.a = value;
             _backface.FillColorStart = backfaceColor;
             _backface.FillColorEnd = backfaceColor;
 
-            _strokeThickness = Mathf.Lerp(0, _strokeThickness, value);
+            _stroke.Thickness = Mathf.Lerp(0, _strokeThickness, value);
 
             textColor.a = value;
             _text.color = textColor;
         });
-        _showTween.SetEase(Ease.OutCubic);
+        _showTween.SetAutoKill(false);
         _showTween.Pause();
 
+        _hideTween = DOVirtual.Float(1, 0, _hideDuration, value =>
+        {
+            backfaceColor.a = value;
+            _backface.FillColorStart = backfaceColor;
+            _backface.FillColorEnd = backfaceColor;
+
+            _stroke.Thickness = Mathf.Lerp(0, _strokeThickness, value);
+
+            textColor.a = value;
+            _text.color = textColor;
+        });
+        _hideTween.SetAutoKill(false);
+        _hideTween.Pause();
 
     }
 
