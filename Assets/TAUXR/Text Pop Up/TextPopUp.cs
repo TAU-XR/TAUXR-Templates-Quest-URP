c6c23493184781c21a36071a1ecfc9a4c822dab4
Nitsan <nitsan447@gmail.com>
2024-04-15 18:36:51 +0300

    Working on text pop up

--- Assets/TAUXR/Text Pop Up/TextPopUp.cs
+++ Assets/TAUXR/Text Pop Up/TextPopUp.cs
@@ -1,25 +1,34 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using TMPro;
 
-//TODO: Do the animation in code
 public class TextPopUp : MonoBehaviour
 {
-    private const float ScaleMultiplierScalar = 0.5f;
-    private const float DefaultTextWidth = 0.48f;
-    private const float DefaultTextHeight = 0.08f;
+    private float _defaultTextWidth;
+    private float _defaultTextHeight;
     private const int NumberOfLettersWhenScaleIsOne = 55;
 
     [SerializeField] private bool _useAnimation = true;
 
-    [SerializeField] private GameObject _backFace;
-    [SerializeField] private GameObject _pointer;
+    [SerializeField] private Transform _background;
     [SerializeField] private TextMeshPro _textUI;
-
+    private RectTransform _textRectTransform;
 
     [TextArea(1, 10)] [SerializeField] private string _text;
+
+    private void Awake()
+    {
+        _textRectTransform = _textUI.gameObject.GetComponent<RectTransform>();
+    }
+
+    private void Start()
+    {
+        _defaultTextWidth = _textRectTransform.rect.width;
+        _defaultTextHeight = _textRectTransform.rect.height;
+    }
 
     private void OnEnable()
     {
@@ -27,11 +36,6 @@ public class TextPopUp : MonoBehaviour
         {
             GetComponent<Animator>().Play("Birth", -1, 1);
         }
-    }
-
-    public void GetTextFromComponent()
-    {
-        _text = _textUI.text;
     }
 
     public void SetText(string newText)
@@ -42,70 +46,40 @@ public class TextPopUp : MonoBehaviour
     public void SetTextAndScale(string newText)
     {
         _textUI.text = newText;
-        SetNewScale();
-    }
-
-    public void SetTextAndScaleFromSerializedField()
-    {
-        _textUI.text = _text;
-        SetNewScale();
-    }
-
-
-    public void SetNewScale()
-    {
-        float scaleMultiplier = CalculateScaleMultiplier();
-        UpdateBackFace(scaleMultiplier);
-        UpdateTextUI(scaleMultiplier);
-        UpdatePointer(scaleMultiplier);
-    }
-
-
-    private float CalculateScaleMultiplier()
-    {
-        float scaleMultiplier = (float)_textUI.text.Length / NumberOfLettersWhenScaleIsOne;
-        scaleMultiplier = Mathf.Lerp(1, scaleMultiplier, ScaleMultiplierScalar);
-        if (_textUI.text.Length > 300)
-        {
-            scaleMultiplier /= 1.5f;
-        }
-
-        return scaleMultiplier;
-    }
-
-    private void UpdateBackFace(float scaleMultiplier)
-    {
-        //TODO: instead of changing scale, change rectangle size
-        _backFace.transform.localScale = new Vector3(scaleMultiplier, scaleMultiplier, 1);
     }
 
     private void UpdateTextUI(float scaleMultiplier)
     {
-        _textUI.rectTransform.sizeDelta = new Vector2(DefaultTextWidth * scaleMultiplier,
-            DefaultTextHeight * scaleMultiplier);
+        _textUI.rectTransform.sizeDelta = new Vector2(_defaultTextWidth * scaleMultiplier,
+            _defaultTextHeight * scaleMultiplier);
     }
 
-    private void UpdatePointer(float scaleMultiplier)
-    {
-        //TODO: instead of changing scale, change rectangle size
-        _pointer.transform.localPosition = new Vector3(-0.0034f, 0,
-            _textUI.rectTransform.offsetMax.x + 0.02f * scaleMultiplier);
-        _pointer.transform.localScale = new Vector3(scaleMultiplier, scaleMultiplier, 1);
-    }
 
 #if UNITY_EDITOR
+    [Button]
+    public void GetTextFromComponent()
+    {
+        _text = _textUI.text;
+    }
+
+    [Button]
+    public void SetTextAndScaleFromSerializedField()
+    {
+        _textUI.text = _text;
+    }
+
+    [Button]
+    public void DebugNumberOfLetters()
+    {
+        Debug.Log(_textUI.text.Length);
+    }
+
+    [Button]
     public void ResetScale()
     {
         _textUI.rectTransform.sizeDelta = new Vector2(0.48f, 0.08f);
-        _pointer.transform.localPosition = new Vector3(-0.0034f, 0, 0.26f);
-        _pointer.transform.localScale = Vector3.one;
-        _backFace.transform.localScale = Vector3.one;
+        _background.transform.localScale = Vector3.one;
     }
 
-    public void DebugNumberOfLettersAndScaleMultiplier()
-    {
-        Debug.Log(_textUI.text.Length);
-        Debug.Log(CalculateScaleMultiplier());
-    }
 #endif
 }
