f0a6d4a2b2ff65da1d13b67e3cb9a791141ee549
Nitsan <nitsan447@gmail.com>
2024-04-17 19:12:26 +0300

    Extracted scaling functionality to text auto scaler

--- Assets/TAUXR/Text Pop Up/TextPopUp.cs
+++ Assets/TAUXR/Text Pop Up/TextPopUp.cs
@@ -8,31 +8,13 @@ using TMPro;
 
 public class TextPopUp : MonoBehaviour
 {
-    private const float DefaultTextFontSize = 0.34f;
-    private const int ReferenceNumberOfLettersUntilLineWrap = 74;
-    private const int ReferenceNumberOfLetters = 300;
-    private const float ReferenceSquareMeters = 0.25f;
-    private const float ReferenceLineHeight = 0.04f;
-
-    [SerializeField] private bool _useAnimation = true;
-    [SerializeField] private Rectangle _background;
     [SerializeField] private TextMeshPro _textUI;
-
-    [SerializeField] private float _fontSizeMultiplier = 1;
-    [SerializeField] private float _layoutRatio = 5.5f;
-    [SerializeField] private float _scaleFactor = 1;
-    [SerializeField] private bool _extendWidthOnly;
-    [SerializeField] private bool _extendHeightOnly;
-
     [TextArea(1, 10)] [SerializeField] private string _text;
-
-    private Vector2 _scale;
-    [SerializeField] private Vector2 _backgroundPadding = new(0.3f, 0.1f);
-    [SerializeField] private bool _autoScaleWhenChangingText = true;
+    [SerializeField] private TextAutoScaler _textAutoScaler;
 
     private void OnValidate()
     {
-        if (Application.isPlaying || !_autoScaleWhenChangingText)
+        if (Application.isPlaying || !_textAutoScaler.AutoScaleWhenChangingText)
         {
             return;
         }
@@ -45,72 +27,12 @@ public class TextPopUp : MonoBehaviour
         _textUI.text = newText;
     }
 
-
     public void SetTextAndScale(string newText)
     {
         _textUI.text = newText;
-        _textUI.fontSize = DefaultTextFontSize * _fontSizeMultiplier;
-
-        float xScale = Mathf.Sqrt(_layoutRatio * ReferenceSquareMeters);
-        float yScale = Mathf.Sqrt(1 / _layoutRatio * ReferenceSquareMeters) + GetNumberOfExtraLineBreaks() * ReferenceLineHeight;
-        _scale = new Vector2(xScale, yScale) * _scaleFactor * GetLayoutScalingFactor();
-
-        _textUI.rectTransform.sizeDelta = _scale;
-        Vector2 backFaceSize = _scale + _backgroundPadding;
-        _background.Width = backFaceSize.x;
-        _background.Height = backFaceSize.y;
+        _textAutoScaler.Text = newText;
+        _textAutoScaler.SetScale();
     }
-
-    private Vector2 GetLayoutScalingFactor()
-    {
-        float scalingFactor = (float)_text.Length / ReferenceNumberOfLetters;
-        float newScaleX;
-        float newScaleY;
-
-        if (_extendWidthOnly)
-        {
-            newScaleX = scalingFactor;
-            newScaleY = 1;
-        }
-        else if (_extendHeightOnly)
-        {
-            newScaleY = scalingFactor;
-            newScaleX = 1;
-        }
-        else
-        {
-            newScaleX = Mathf.Sqrt(scalingFactor);
-            newScaleY = Mathf.Sqrt(scalingFactor);
-        }
-
-        Vector2 newScale = new Vector2(newScaleX, newScaleY) * _fontSizeMultiplier;
-
-        return newScale;
-    }
-
-    private int GetNumberOfExtraLineBreaks()
-    {
-        return GetNumberOfLineBreaks() - GetNumberOfWrappingLineBreaksInText(_text);
-    }
-
-    private int GetNumberOfLineBreaks()
-    {
-        string[] paragraphs = _text.Split("\n");
-        int manualLineBreaks = paragraphs.Length - 1;
-        int wrappingLineBreaksInParagraph = 0;
-        foreach (string paragraph in paragraphs)
-        {
-            wrappingLineBreaksInParagraph += GetNumberOfWrappingLineBreaksInText(paragraph);
-        }
-
-        return manualLineBreaks + wrappingLineBreaksInParagraph;
-    }
-
-    private int GetNumberOfWrappingLineBreaksInText(string text)
-    {
-        return text.Length / (int)(ReferenceNumberOfLettersUntilLineWrap * (float)_text.Length / ReferenceNumberOfLetters);
-    }
-
 
 #if UNITY_EDITOR
     [Button]
@@ -124,19 +46,5 @@ public class TextPopUp : MonoBehaviour
     {
         SetTextAndScale(_text);
     }
-
-    [Button]
-    public void DebugNumberOfLetters()
-    {
-        Debug.Log(_textUI.text.Length);
-    }
-
-    [Button]
-    public void ResetScale()
-    {
-        _textUI.rectTransform.sizeDelta = new Vector2(0.48f, 0.08f);
-        _background.transform.localScale = Vector3.one;
-    }
-
 #endif
 }
---
+++ Assets/TextAutoScaler.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Shapes;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using TMPro;
+
+public class TextAutoScaler : MonoBehaviour
+{
+    private const float DefaultTextFontSize = 0.34f;
+    private const int ReferenceNumberOfLettersUntilLineWrap = 74;
+    private const int ReferenceNumberOfLetters = 300;
+    private const float ReferenceSquareMeters = 0.25f;
+    private const float ReferenceLineHeight = 0.04f;
+
+    [SerializeField] private Rectangle _background;
+    [SerializeField] private TextMeshPro _textUI;
+
+    [SerializeField] private float _fontSizeMultiplier = 1;
+    [SerializeField] private float _layoutRatio = 5.5f;
+    [SerializeField] private float _scaleFactor = 1;
+    [SerializeField] private bool _extendWidthOnly;
+    [SerializeField] private bool _extendHeightOnly;
+
+    [SerializeField] private Vector2 _backgroundPadding = new(0.3f, 0.1f);
+    public bool AutoScaleWhenChangingText => _autoScaleWhenChangingText;
+    [SerializeField] private bool _autoScaleWhenChangingText = true;
+
+    [HideInInspector] public string Text;
+
+
+    public void SetScale()
+    {
+        _textUI.fontSize = DefaultTextFontSize * _fontSizeMultiplier;
+
+        float xScale = Mathf.Sqrt(_layoutRatio * ReferenceSquareMeters);
+
+        float heightAdditionFromExtraLineBreaks = GetNumberOfExtraLineBreaks() * ReferenceLineHeight;
+        float yScale = Mathf.Sqrt(1 / _layoutRatio * ReferenceSquareMeters) + heightAdditionFromExtraLineBreaks;
+
+        Vector2 scale = new Vector2(xScale, yScale) * _scaleFactor * _fontSizeMultiplier * GetNumberOfLettersScalingFactor();
+
+        _textUI.rectTransform.sizeDelta = scale;
+        Vector2 backFaceSize = scale + _backgroundPadding;
+        _background.Width = backFaceSize.x;
+        _background.Height = backFaceSize.y;
+    }
+
+    private Vector2 GetNumberOfLettersScalingFactor()
+    {
+        float squareMetersScalingFactor = (float)Text.Length / ReferenceNumberOfLetters;
+        float newScaleX = 1;
+        float newScaleY = 1;
+
+        if (_extendWidthOnly)
+        {
+            newScaleX = squareMetersScalingFactor;
+        }
+        else if (_extendHeightOnly)
+        {
+            newScaleY = squareMetersScalingFactor;
+        }
+        else
+        {
+            newScaleX = Mathf.Sqrt(squareMetersScalingFactor);
+            newScaleY = Mathf.Sqrt(squareMetersScalingFactor);
+        }
+
+        Vector2 newScale = new(newScaleX, newScaleY);
+
+        return newScale;
+    }
+
+    private int GetNumberOfExtraLineBreaks()
+    {
+        return GetNumberOfLineBreaks() - GetNumberOfWrappingLineBreaksInText(Text);
+    }
+
+    private int GetNumberOfLineBreaks()
+    {
+        string[] paragraphs = Text.Split("\n");
+        int manualLineBreaks = paragraphs.Length - 1;
+        int wrappingLineBreaksInParagraph = 0;
+        foreach (string paragraph in paragraphs)
+        {
+            wrappingLineBreaksInParagraph += GetNumberOfWrappingLineBreaksInText(paragraph);
+        }
+
+        return manualLineBreaks + wrappingLineBreaksInParagraph;
+    }
+
+    private int GetNumberOfWrappingLineBreaksInText(string text)
+    {
+        return text.Length / (int)(ReferenceNumberOfLettersUntilLineWrap * (float)Text.Length / ReferenceNumberOfLetters);
+    }
+
+
+#if UNITY_EDITOR
+    [Button]
+    public void DebugNumberOfLetters()
+    {
+        Debug.Log(_textUI.text.Length);
+    }
+#endif
+}
