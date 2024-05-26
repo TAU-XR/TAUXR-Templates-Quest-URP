3e76fd818e8821a4b44657fd2a31e1a83253ddbb
Nitsan <nitsan447@gmail.com>
2024-05-26 14:53:41 +0300

    Worked on SQI manager

---
+++ Assets/TAUXR/SelectionQuestionInterface/Editor/SelectionQuestionInterfaceManagerEditor.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(SelectionQuestionInterfaceManager))]
+public class SelectionQuestionInterfaceManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        
+        
+    }
+}
--- Assets/TAUXR/SelectionQuestionInterface/SelectionQuestionInterfaceManager.cs
+++ Assets/TAUXR/SelectionQuestionInterface/SelectionQuestionInterfaceManager.cs
@@ -5,17 +5,32 @@ using Cysharp.Threading.Tasks;
 using NaughtyAttributes;
 using UnityEngine;
 
-public class SelectionQuestionInterfaceTester : MonoBehaviour
+public class SelectionQuestionInterfaceManager : MonoBehaviour
 {
     public SelectionQuestionData[] SelectionQuestions;
     [SerializeField] private float _timeBetweenQuestions = 5;
+    private int _currentQuestionIndex = 0;
 
-    private async void Start()
+    private void Start()
+    {
+        RunExam().Forget();
+    }
+
+    private async UniTask RunExam()
     {
         foreach (SelectionQuestionData selectionQuestion in SelectionQuestions)
         {
             await GetComponent<SelectionQuestionInterface>().ShowQuestionAndWaitForFinalSubmission(selectionQuestion);
             await UniTask.Delay(TimeSpan.FromSeconds(_timeBetweenQuestions));
         }
+    }
+
+    public void NextQuestion()
+    {
+        
+    }
+
+    public void PreviousQuestion()
+    {
     }
 }
