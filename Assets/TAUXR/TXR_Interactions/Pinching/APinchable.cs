7a663c14a4a692d1f0330aa180fc418af80f18f8
Nitsan <nitsan447@gmail.com>
2024-07-17 16:47:32 +0300

    Fixes to pinching

--- Assets/TAUXR/TXR_Interactions/Pinching/APinchable.cs
+++ Assets/TAUXR/TXR_Interactions/Pinching/APinchable.cs
@@ -6,95 +6,94 @@ using UnityEngine;
 
 public abstract class APinchable : MonoBehaviour, IComparable<APinchable>
 {
-	public PinchManager PinchingHandPinchManager { get; set; }
+    public PinchManager PinchingHandPinchManager { get; set; }
 
-	public virtual bool IsUsedByPinchMeIndicator => true;
-	public virtual float PinchExitThreshold => 0.97f;
-	public virtual int Priority => 0;
-	public virtual Vector3 PinchMeIndicatorLineStartPosition { get; set; }
+    public virtual bool IsUsedByPinchMeIndicator => true;
+    public virtual float PinchExitThreshold => 0.97f;
+    public virtual int Priority => 0;
+    public virtual Vector3 PinchMeIndicatorLineStartPosition { get; set; }
 
-	protected int _numberOfPinchersInside = 0;
-	protected AHoverEffect _hoverEffect;
+    protected int _numberOfPinchersInside = 0;
+    protected AHoverEffect _hoverEffect;
 
-	private void Awake()
-	{
-		_hoverEffect = GetComponent<AHoverEffect>();
-		DoOnAwake();
-	}
+    private void Awake()
+    {
+        _hoverEffect = GetComponent<AHoverEffect>();
+        DoOnAwake();
+    }
 
-	protected virtual void DoOnAwake()
-	{
+    protected virtual void DoOnAwake()
+    {
+    }
 
-	}
+    public virtual void OnHoverEnter(PinchManager pinchManager)
+    {
+        _numberOfPinchersInside++;
+    }
 
-	public virtual void OnHoverEnter(PinchManager pinchManager)
-	{
-		_numberOfPinchersInside++;
-	}
+    public virtual void OnHoverStay(PinchManager pinchManager)
+    {
+        ToggleHoverEffectState(pinchManager, ShouldEffectBeActiveOnHover(pinchManager));
+    }
 
-	public virtual void OnHoverStay(PinchManager pinchManager)
-	{
-		ToggleHoverEffectState(pinchManager, ShouldEffectBeActiveOnHover(pinchManager));
-	}
+    public virtual bool ShouldEffectBeActiveOnHover(PinchManager pinchManager)
+    {
+        return true;
+    }
 
-	public virtual bool ShouldEffectBeActiveOnHover(PinchManager pinchManager)
-	{
-		return true;
-	}
+    protected void ToggleHoverEffectState(PinchManager pinchManager, bool shouldStartEffect)
+    {
+        if (_hoverEffect == null)
+        {
+            return;
+        }
 
-	protected void ToggleHoverEffectState(PinchManager pinchManager, bool shouldStartEffect)
-	{
-		if (_hoverEffect == null)
-		{
-			return;
-		}
+        if (shouldStartEffect)
+        {
+            _hoverEffect.Activate(pinchManager);
+        }
+        else
+        {
+            _hoverEffect.Deactivate(pinchManager);
+        }
+    }
 
-		if (shouldStartEffect)
-		{
-			_hoverEffect.Activate(pinchManager);
-		}
-		else
-		{
-			_hoverEffect.Deactivate(pinchManager);
-		}
-	}
+    public virtual void OnHoverExit(PinchManager pinchManager)
+    {
+        _numberOfPinchersInside--;
+        ToggleHoverEffectState(pinchManager, _numberOfPinchersInside > 0);
+    }
 
-	public virtual void OnHoverExit(PinchManager pinchManager)
-	{
-		_numberOfPinchersInside--;
-		ToggleHoverEffectState(pinchManager, _numberOfPinchersInside > 0);
-	}
+    public virtual bool CanBePinched(PinchManager pinchManager)
+    {
+        return true;
+    }
 
-	public virtual bool CanBePinched(PinchManager pinchManager)
-	{
-		return true;
-	}
+    public virtual void OnPinchEnter(PinchManager pinchManager)
+    {
+        PinchingHandPinchManager = pinchManager;
+        pinchManager.PinchedObject = this;
+    }
 
-	public virtual void OnPinchEnter(PinchManager pinchManager)
-	{
-		PinchingHandPinchManager = pinchManager;
-		pinchManager.PinchedObject = this;
-	}
+    public virtual void OnPinchExit()
+    {
+        PinchingHandPinchManager.PinchedObject = null;
+        PinchingHandPinchManager = null;
+    }
 
-	public virtual void OnPinchExit()
-	{
-		PinchingHandPinchManager.PinchedObject = null;
-		PinchingHandPinchManager = null;
-	}
+    public virtual void UpdatePinchMeIndicatorLineStartPosition()
+    {
+        PinchMeIndicatorLineStartPosition = transform.position;
+    }
 
-	public virtual void UpdatePinchMeIndicatorLineStartPosition()
-	{
-		PinchMeIndicatorLineStartPosition = transform.position;
-	}
+    public int CompareTo(APinchable other)
+    {
+        //Using int.CompareTo - which is different than IComparable CompareTo method
+        return other.Priority.CompareTo(Priority);
+    }
 
-	public int CompareTo(APinchable other)
-	{
-		//Using int.CompareTo - which is different than IComparable CompareTo method
-		return other.Priority.CompareTo(Priority);
-	}
-
-	protected bool IsOtherHand(PinchManager pinchManager)
-	{
-		return PinchingHandPinchManager != null && PinchingHandPinchManager != pinchManager;
-	}
+    protected bool IsOtherHand(PinchManager pinchManager)
+    {
+        return PinchingHandPinchManager != null && PinchingHandPinchManager != pinchManager;
+    }
 }
--- Assets/TAUXR/TXR_Interactions/Pinching/PinchManager.cs
+++ Assets/TAUXR/TXR_Interactions/Pinching/PinchManager.cs
@@ -5,147 +5,144 @@ using System;
 
 public class PinchManager
 {
-	public Action<PinchManager> PinchEnter, PinchExit;
-	private readonly List<APinchable> _pinchablesInRange = new();
-	[HideInInspector] public APinchable PinchedObject;
+    public Action<PinchManager> PinchEnter, PinchExit;
+    private readonly SortedSet<APinchable> _pinchablesInRange = new();
+    [HideInInspector] public APinchable PinchedObject;
 
-	private bool _isPinching = false;
-	private bool _wasPinchingLastFrame = false;
-	private bool _pinchOccuredThisFrame => !_wasPinchingLastFrame && _isPinching;
+    private bool _isPinching = false;
+    private bool _wasPinchingLastFrame = false;
+    private bool _pinchOccuredThisFrame => !_wasPinchingLastFrame && _isPinching;
 
-	private float _timeSinceLastPinch;
+    private float _timeSinceLastPinch;
 
-	public PinchingConfiguration Configuration => _configuration;
-	private PinchingConfiguration _configuration;
-	public Pincher Pincher => _pincher;
-	private readonly Pincher _pincher;
-	public HandType HandType => _handType;
-	private readonly HandType _handType;
+    public PinchingConfiguration Configuration => _configuration;
+    private PinchingConfiguration _configuration;
+    public Pincher Pincher => _pincher;
+    private readonly Pincher _pincher;
+    public HandType HandType => _handType;
+    private readonly HandType _handType;
 
-	public PinchManager(TXRHand hand, PinchingConfiguration pinchingConfiguration)
-	{
-		_handType = hand.HandType;
-		_pincher = hand.Pincher;
-		_configuration = pinchingConfiguration;
-		_timeSinceLastPinch = _configuration.MinimumTimeBetweenPinches;
-	}
+    public PinchManager(TXRHand hand, PinchingConfiguration pinchingConfiguration)
+    {
+        _handType = hand.HandType;
+        _pincher = hand.Pincher;
+        _configuration = pinchingConfiguration;
+        _timeSinceLastPinch = _configuration.MinimumTimeBetweenPinches;
+    }
 
-	public void HandlePinching()
-	{
-		//Prevent double pinching
-		if (_timeSinceLastPinch < _configuration.MinimumTimeBetweenPinches)
-		{
-			_timeSinceLastPinch += Time.deltaTime;
-		}
+    public void HandlePinching()
+    {
+        //Prevent double pinching
+        if (_timeSinceLastPinch < _configuration.MinimumTimeBetweenPinches)
+        {
+            _timeSinceLastPinch += Time.deltaTime;
+        }
 
-		_pincher.UpdatePincher();
+        _pincher.UpdatePincher();
 
-		HandlePinchEvents();
+        HandlePinchEvents();
 
-		foreach (APinchable pinchable in _pinchablesInRange)
-		{
-			pinchable.OnHoverStay(this);
-		}
+        foreach (APinchable pinchable in _pinchablesInRange)
+        {
+            pinchable.OnHoverStay(this);
+        }
 
-		if (_pinchOccuredThisFrame && PinchedObject == null)
-		{
-			APinchable objectToPinch = ChooseObjectToPinch();
+        if (_pinchOccuredThisFrame && PinchedObject == null)
+        {
+            APinchable objectToPinch = ChooseObjectToPinch();
 
-			if (objectToPinch != null)
-			{
-				PinchObject(objectToPinch);
-			}
-		}
+            if (objectToPinch != null)
+            {
+                PinchObject(objectToPinch);
+            }
+        }
 
-		if (PinchedObject != null && _pincher.Strength <= PinchedObject.PinchExitThreshold)
-		{
-			PinchedObject.OnPinchExit();
-		}	
-	}
-	
-	private void HandlePinchEvents()
-	{
-		_wasPinchingLastFrame = _isPinching;
-		if (!_isPinching)
-		{
-			bool nextPinchReady = _timeSinceLastPinch >= _configuration.MinimumTimeBetweenPinches;
-			if (_pincher.Strength > _configuration.PinchEnterThreshold && nextPinchReady)
-			{
-				_timeSinceLastPinch = 0;
-				_isPinching = true;
-				PinchEnter?.Invoke(this);
-			}
-		}
-		else
-		{
-			if (_pincher.Strength < _configuration.PinchExitThreshold)
-			{
-				_isPinching = false;
-				PinchExit?.Invoke(this);
-			}
-		}
-	}
+        if (PinchedObject != null && _pincher.Strength <= PinchedObject.PinchExitThreshold)
+        {
+            PinchedObject.OnPinchExit();
+        }
+    }
 
-	public APinchable ChooseObjectToPinch()
-	{
-		foreach (APinchable pinchable in _pinchablesInRange)
-		{
-			if (pinchable.CanBePinched(this))
-			{
-				return pinchable;
-			}
-		}
+    private void HandlePinchEvents()
+    {
+        _wasPinchingLastFrame = _isPinching;
+        if (!_isPinching)
+        {
+            bool nextPinchReady = _timeSinceLastPinch >= _configuration.MinimumTimeBetweenPinches;
+            if (_pincher.Strength > _configuration.PinchEnterThreshold && nextPinchReady)
+            {
+                _timeSinceLastPinch = 0;
+                _isPinching = true;
+                PinchEnter?.Invoke(this);
+            }
+        }
+        else
+        {
+            if (_pincher.Strength < _configuration.PinchExitThreshold)
+            {
+                _isPinching = false;
+                PinchExit?.Invoke(this);
+            }
+        }
+    }
 
-		return null;
-	}
+    public APinchable ChooseObjectToPinch()
+    {
+        foreach (APinchable pinchable in _pinchablesInRange)
+        {
+            if (pinchable.CanBePinched(this))
+            {
+                return pinchable;
+            }
+        }
 
-	public APinchable ChooseInteractablePinchable()
-	{
-		foreach (APinchable pinchable in _pinchablesInRange)
-		{
-			if (pinchable.ShouldEffectBeActiveOnHover(this) && pinchable.IsUsedByPinchMeIndicator)
-			{
-				return pinchable;
-			}
-		}
+        return null;
+    }
 
-		return null;
-	}
+    public APinchable ChooseInteractablePinchable()
+    {
+        foreach (APinchable pinchable in _pinchablesInRange)
+        {
+            if (pinchable.ShouldEffectBeActiveOnHover(this) && pinchable.IsUsedByPinchMeIndicator)
+            {
+                return pinchable;
+            }
+        }
 
-	private void PinchObject(APinchable objectToPinch)
-	{
-		objectToPinch.OnPinchEnter(this);
-	}
+        return null;
+    }
 
-	public void AddPinchableInRange(APinchable pinchable)
-	{
-		_pinchablesInRange.Add(pinchable);
+    private void PinchObject(APinchable objectToPinch)
+    {
+        objectToPinch.OnPinchEnter(this);
+    }
 
-		//If sort becomes expensive, do it in a more optimized way (priority queue?)
-		_pinchablesInRange.Sort();
-	}
+    public void AddPinchableInRange(APinchable pinchable)
+    {
+        _pinchablesInRange.Add(pinchable);
+    }
 
-	public void RemovePinchableInRange(APinchable pinchable)
-	{
-		_pinchablesInRange.Remove(pinchable);
-	}
+    public void RemovePinchableInRange(APinchable pinchable)
+    {
+        _pinchablesInRange.Remove(pinchable);
+    }
 
-	//Called from pinchables, that want to know if a certain other pinchable is in range.
-	public APinchable FindPinchableByType(Type pinchableType)
-	{
-		foreach (APinchable pinchable in _pinchablesInRange)
-		{
-			if (pinchable.GetType() == pinchableType)
-			{
-				return pinchable;
-			}
-		}
+    //Called from pinchables, that want to know if a certain other pinchable is in range.
+    public APinchable FindPinchableByType(Type pinchableType)
+    {
+        foreach (APinchable pinchable in _pinchablesInRange)
+        {
+            if (pinchable.GetType() == pinchableType)
+            {
+                return pinchable;
+            }
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	public bool IsPlayerPinchingThisFrame()
-	{
-		return _isPinching;
-	}
+    public bool IsPlayerPinchingThisFrame()
+    {
+        return _isPinching;
+    }
 }
